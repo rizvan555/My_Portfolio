aec4e419e810d9af0f43456f99241fe6ce5f9b6f
Rizvan Karimov <rizvan84@gmx.de>
2023-05-07 19:48:24 +0200

    Update page4.tsx

--- src/pages/page4.tsx
+++ src/pages/page4.tsx
@@ -4,9 +4,9 @@ import 'animate.css/animate.min.css';
 function Page4() {
   const experiances = [
     {
-      title: 'Logistician',
+      title: 'Productionsentwickler',
       company: 'IK Hofmann GmbH',
-      date: 'Nivember 2021 -  December 2022',
+      date: 'November 2021 -  December 2022',
       contain:
         'I was responsible for ensuring the safe acceptance of containers, their inspection by me after being filled by other employees, and their proper placement to the appropriate locations at BASF, the largest chemical company. I worked with great attention to detail in these tasks and took all necessary precautions to ensure that the job was done correctly. I was meticulous in ensuring that the containers were properly labeled, marked, and stored or transported in a manner that prevented damage to the products.  In addition, I worked closely with my colleagues to ensure that the job was completed on time and without any issues. I always took care to protect the satisfaction of customers and the reputation of the company',
     },
@@ -20,7 +20,7 @@ function Page4() {
     {
       title: 'Front End Developer',
       company: 'Farkhadov LLC & Trans Broker MMC',
-      date: 'Oktober 2006 - September 2015',
+      date: 'October 2006 - September 2015',
       contain:
         'During my tenure from 2006 to 2015, I had the opportunity to work as a Frontend Developer in various joint projects with Farkhadov LLC and Transbroker LLC. My role involved utilizing my expertise in HTML, CSS, JavaScript, and MySQL that I had acquired during my university studies. I was able to further develop these skills in a professional setting, working on diverse projects.As part of my responsibilities, I was responsible for monitoring the websites of various government agencies and private companies. This included ensuring the proper functioning of webpages, as well as managing and organizing the necessary data. I was also tasked with updating outdated information with the latest data to ensure that users had access to the most up-to-date information.Overall, my experience working in this role allowed me to hone my technical skills, learn to work collaboratively with colleagues, and develop an understanding of how to manage and maintain websites effectively.',
     },
