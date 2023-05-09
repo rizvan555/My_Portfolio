5df0f7ccf61b13bc04d313e9f086ffff3dc185ba
Rizvan Karimov <rizvan84@gmx.de>
2023-05-09 09:02:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './pages/footer';
 import Page1 from './pages/page1';
 import Page2 from './pages/page2';
@@ -8,15 +8,17 @@ import Page5 from './pages/page5';
 import Page6 from './pages/page6';
 
 function App() {
+  const [language, setLanguage] = useState(true);
+
   return (
     <div>
-      <Page1 />
-      <Page2 />
+      <Page1 language={language} setLanguage={setLanguage} />
+      <Page2 language={language} setLanguage={setLanguage} />
       <Page3 />
       <Page4 />
       <Page5 />
       <Page6 />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
