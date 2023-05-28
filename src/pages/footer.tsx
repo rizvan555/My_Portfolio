8f9ff1081ba639d27340956df9ce2460da0886f0
RizvanKarimov <karimovrizvan84@gmail.com>
2023-05-28 14:11:44 +0200

    responsive are ready

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -35,12 +35,12 @@ function Footer({ language }: FooterProps) {
 
   return (
     <div className="flex justify-center bg-[#100c18] h-[100vh] md:p-4 flex-col border-t-2">
-      <main className="flex justify-around flex-wrap md:mt-0 mt-28">
+      <main className="flex justify-around flex-wrap md:mt-0 mt-40 md:mb-0 mb-20">
         <form
           className="flex flex-col gap-7 md:w-[40vw] w-[80vw]"
           onSubmit={formik.handleSubmit}
         >
-          <h3 className=" md:mt-19 text-slate-50 font-bold tracking-wider md:text-xl">
+          <h3 className=" mt-19 text-slate-50 font-bold tracking-wider md:text-xl">
             {language ? eng.footerTitle : de.footerTitle}
           </h3>
           {language
