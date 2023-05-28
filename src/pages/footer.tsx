d8906a40e68c0d9f44ec58a6cac8abc02a087e3d
RizvanKarimov <karimovrizvan84@gmail.com>
2023-05-28 13:11:36 +0200

    responsive footer page is ready

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -34,10 +34,13 @@ function Footer({ language }: FooterProps) {
   };
 
   return (
-    <div className="flex justify-center bg-[#100c18] h-[100vh] p-6 flex-col border-t-2">
-      <main className="flex justify-around">
-        <form className="flex flex-col gap-7" onSubmit={formik.handleSubmit}>
-          <h3 className=" mt-19 text-slate-50 font-bold tracking-wider text-xl">
+    <div className="flex justify-center bg-[#100c18] h-[100vh] md:p-4 flex-col border-t-2">
+      <main className="flex justify-around flex-wrap md:mt-0 mt-28">
+        <form
+          className="flex flex-col gap-7 md:w-[40vw] w-[80vw]"
+          onSubmit={formik.handleSubmit}
+        >
+          <h3 className=" md:mt-19 text-slate-50 font-bold tracking-wider md:text-xl">
             {language ? eng.footerTitle : de.footerTitle}
           </h3>
           {language
@@ -45,7 +48,7 @@ function Footer({ language }: FooterProps) {
                 return (
                   <input
                     key={index}
-                    className=" bg-transparent placeholder:text-slate-50 border-x-0 border-t-0 text-slate-50"
+                    className=" bg-transparent placeholder:text-slate-50 border-x-0 border-t-0 text-slate-50 md:text-lg text-[12px]"
                     type={input.type}
                     placeholder={input.placeholder}
                     onChange={formik.handleChange}
@@ -78,13 +81,14 @@ function Footer({ language }: FooterProps) {
           ></textarea>
 
           <button
-            className=" w-[20vw] p-4 text-slate-900 bg-slate-50 font-bold hvr-radial-out border"
+            className=" md:w-[20vw] w-[40vw] md:p-4 p-2 text-slate-900 bg-slate-50 font-bold hvr-radial-out border md:mx-0 mx-auto"
             type="submit"
           >
             {language ? eng.messageButton : de.messageButton}
           </button>
         </form>
-        <section className=" w-60 flex flex-col mt-20 text-slate-50">
+
+        <section className=" flex flex-col md:w-72 w-[80vw] mt-20 text-slate-50 md:text-start text-center">
           <h3 className="mb-10 font-bold tracking-wider text-xl">
             {language ? eng.contactMe : de.contactMe}
           </h3>
@@ -96,7 +100,7 @@ function Footer({ language }: FooterProps) {
 
           <section className="mb-14">
             <a
-              className="border p-3 w-[20vw] mt-10 hvr-radial-out text-center"
+              className="border md:p-3 p-2 md:w-[20vw] w-[40vw] mt-10 hvr-radial-out text-center"
               style={{ backgroundColor: "transparent" }}
               download="Lebenslauf.pdf"
               href="/Lebenslauf.pdf"
