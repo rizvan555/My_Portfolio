3f23ff818f7f82d1ca27a97467ace4aca5edfb4c
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-06 09:31:54 +0200

    changed function typewriting in Home page

--- src/components/TypeWrite.tsx
+++ src/components/TypeWrite.tsx
@@ -2,15 +2,17 @@ import de from "../components/de.json";
 import eng from "../components/eng.json";
 import myPhoto5 from "../resource/images/myPhoto5.png";
 import Typewriter from "typewriter-effect";
+
 interface TypeWriteProps {
   language: boolean;
 }
 
 function TypeWrite({ language }: TypeWriteProps) {
   return (
-    <main className={`flex relative`}>
-      <section className=" md:w-[50vw] w-[80vw] md:mt-[38vh] my-44 md:ml-24 ml-10 font-[Neuemachina,sans-serif]">
-        <h1 className="md:text-6xl w-full text-3xl h-20 md:mb-8 mb-0 mt-20 text-slate-200/100 font-serif tracking-wide animate__animated overflow-hidden border-r-orange-500 whitespace-nowrap">
+    <main className="flex relative">
+      <section className=" md:w-[60vw] w-[80vw] md:mt-[38vh] my-44 md:ml-24 ml-10 font-[Neuemachina,sans-serif]">
+        <h1 className="md:text-5xl w-full text-3xl md:mb-8 mb-0 mt-20 text-slate-200/100 font-serif tracking-wide animate__animated overflow-hidden border-r-orange-500 whitespace-nowrap flex gap-4 h-20">
+          <p className="text-[#9d23ce]">Ich bin</p>
           <Typewriter
             options={{
               strings: [language ? eng.title : de.title],
@@ -24,7 +26,10 @@ function TypeWrite({ language }: TypeWriteProps) {
         <h3 className="mb-11 md:text-lg text-base font-light tracking-wide md:w-[45vw]">
           <Typewriter
             options={{
-              strings: [language ? eng.subtitle : de.subtitle],
+              strings: [
+                language ? eng.subtitle : de.subtitle,
+                language ? eng.subtitle1 : de.subtitle1,
+              ],
               autoStart: true,
               loop: true,
               delay: 50,
