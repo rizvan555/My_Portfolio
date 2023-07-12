e603211a87fd982a7365522b0f97c793284885da
Rizvan <rizvan@MacBook-Air.fritz.box>
2023-07-12 23:28:54 +0200

    added new photo and new lebenslauf

--- src/components/TypeWrite.tsx
+++ src/components/TypeWrite.tsx
@@ -1,7 +1,7 @@
-import de from "../components/de.json";
-import eng from "../components/eng.json";
-import myPhoto5 from "../resource/images/myPhoto5.png";
-import Typewriter from "typewriter-effect";
+import de from '../components/de.json';
+import eng from '../components/eng.json';
+import myNewPhoto from '../resource/images/myNewPhoto.png';
+import Typewriter from 'typewriter-effect';
 
 interface TypeWriteProps {
   language: boolean;
@@ -10,7 +10,7 @@ interface TypeWriteProps {
 function TypeWrite({ language }: TypeWriteProps) {
   return (
     <main className="flex relative">
-      <section className=" md:w-[60vw] w-[80vw] md:mt-[38vh] my-44 md:ml-24 ml-10 font-[Neuemachina,sans-serif]">
+      <section className=" md:w-[65vw] w-[80vw] md:mt-[38vh] my-44 md:ml-12 ml-10 font-[Neuemachina,sans-serif]">
         <h1 className="md:text-5xl w-full text-3xl md:mb-8 mb-0 mt-20 text-slate-200/100 font-serif tracking-wide animate__animated overflow-hidden border-r-orange-500 whitespace-nowrap flex gap-4 h-20">
           <p className="text-[#c555f4] ">
             <span className="text-slate-100 ">{language ? eng.me : de.me}</span>
@@ -28,7 +28,7 @@ function TypeWrite({ language }: TypeWriteProps) {
             }}
           />
         </h1>
-        <h3 className="mb-11 md:text-xl text-base font-light tracking-wide md:w-[50vw] text-[#c555f4]">
+        <h3 className="mb-11 md:text-xl text-base font-light tracking-wide md:w-[55vw] text-[#c555f4]">
           <Typewriter
             options={{
               strings: [
@@ -42,8 +42,8 @@ function TypeWrite({ language }: TypeWriteProps) {
             }}
           />
         </h3>
-        <div className="md:absolute bottom-0 right-0 md:block hidden">
-          <img src={myPhoto5} alt="myPhoto5" width={650} height={650} />
+        <div className="md:absolute bottom-10 right-0 md:block hidden">
+          <img src={myNewPhoto} alt="myPhoto5" width={450} height={450} />
         </div>
       </section>
     </main>
